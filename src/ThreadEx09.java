1e54b8e0505abbc1b8fcf54937d040063c5b7218
faker <good@naver.>
2025-03-18 18:04:34 +0900

    commit

--- src/ThreadEx09.java
+++ src/ThreadEx09.java
@@ -1,61 +1,91 @@
 
-//데몬 스레드를 만들 일반클래스(Runnable인터페이스의 fun메소드를 오버라이딩해서 작성해놓은 클래스)만들기
-class T implements Runnable {
-
-	// 작업파일을 자동저장할것인가 말것인가 여부값 (true, false)를 저장할 플래그 변수
+/*
+	스레드는 크게 2가지 종류로 나뉜다.
+	1. 일반스레드(독립스레드 : main스레드 및 다른 보조작업스레드들) : 
+	   직접 개발자에 의해서 제어가 되는 스레드
+	
+	2. 데몬 스레드 (일반스레드에 의해서 실행되는 종속스레드)
+	  : 일반스레드의 작업을 돕는 보조적인 역할을 하는 스레드 
+	  
+	  데몬스레드?
+	  - 다른 일반스레드의 작업을 돕는 보조적인 일을 수행하는 스레드
+	  - 일반스레드가 모두 종료되면 데몬스레드는 강제로 따라 종료 됩니다.
+	  - 일반스레드를 생성한다음 실행전 setDemon(true); 를 호출하면 일반스레드를 데몬스레드화 해서 만들수 있다
+	  
+	  
+시나리오
+	  어떤 두개의 스레드가 있다고 할떄.....
+	  하나의 스레드(메인스레드)가 종료될떄...
+	  나머지 일반스레드를 데몬스레드로 만들어 자동종료되게 하기
+*/
+//데몬 스레드를 만들 일반클래스(Runnable인터페이스의 run메소드를 오버라이딩해서 작성해놓은 클래스)만들기
+class T implements Runnable{
+	
+	//작업파일을 자동저장할것인가  말것인가 여부값 (true 또는 false)를 저장할 플래그변수
 	static boolean autoSave = false;
-
-	// 작업파일을 자동저장하는 기능을 표시하는 일
+	
+	//작업파일을 자동저장하는 기능을 표시하는일
 	@Override
 	public void run() {
-		// 3초 휴식후 작업파일 저장되었다고 메세지를 반복해서 출력
-		while (true) {
+		//3초 휴식후 작업파일 저장되었다고 메세지 반복해서 출력
+		while(true) {			
 			try {
 				Thread.sleep(3000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
+			}	
+			//autoSave변수에 저장되어 있는 값이 true일때만
+			if(autoSave) {
+				autoSave(); //메소드를 호출하여 작업파일이 자동으로 저장되었다는 메세지 출력
 			}
-
-			// autoSave변수에 저장되어 있는 값이 true일때만
-			if (autoSave) {
-				autoSave(); // 메소드를 호출하여 작업파일이 자동으로 저장되었다는 메세지 출력
-			}
-		} // while
-	}// run
-
+		}//while	
+	}//run()
+	
 	public void autoSave() {
 		System.out.println("작업파일이 자동으로 저장되었습니다.");
-	}
-}// T Class
-
-
+	}	
+}//   T class
 
 public class ThreadEx09 {
-
+	
 	//주(메인)스레드
 	public static void main(String[] args) throws InterruptedException {
 		
 		//보조작업스레드 객체 생성
 		Thread thread = new Thread(new T());
-		
 		//보조작업스레드(일반스레드)를 데몬스레드로 변경
 		thread.setDaemon(true);
-		
 		//데몬스레드 시작
 		thread.start();
 		
 		//(주)메인스레드에서 숫자를 반복해서 출력하는 작업
-		for(int i=0; i<20; i++) {
-			//현재 실행되고 있는 메인스레드 1초 휴식 후 i변수값 출력
+		for(int i=0;  i<=20;  i++) {
+			//현재 실행되고 있는 (주)메인스레드 1초 휴식 후  i변수값 출력
 			Thread.sleep(1000);
 			System.out.println(i);
 			
-//			i변수의 값이 5가 되면 static autoSave변수의 값을 true로 변경해서 저장
-			if(i==5) {
-				T.autoSave=true;
-			}
-		}
-		System.out.println("프로그램종료.");
+			//i변수값이 5가 되는 순간 static autoSave변수의 값을 true로 변경해서 저장
+			if(i == 5) T.autoSave = true;	
+		}//for반복문
+		
+		System.out.println("프로그램 종료합니다.");
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/ThreadEx10.java
+++ src/ThreadEx10.java
@@ -2,8 +2,14 @@
 
 //보조작업스레드 객체를 생성하기 위한 클래스
 //하는일 : 현재 실행중인 스레드 5초 휴식 주는 일
-class ThreadDemo4 extends Thread{
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+class ThreadDemo4 extends Thread  {	
+	
+	//보조작업스레드 객체의 이름을 생성자의 매개변수로 받아 초기화할 생성자
 	public ThreadDemo4(String name) {
 		super(name);
 	}
@@ -11,24 +17,120 @@ class ThreadDemo4 extends Thread{
 	@Override
 	public void run() {
 		try {
-			this.sleep(5*1000);
+			  this.sleep(5*1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	
+	}//run 메소드 닫기 
 }
 
-
-
-
-
+//현재 실행되고 있는 모든 스레드에 대한 정보를 화면에 출력하는 일을 하는 
+//보조작업스레드객체를 생성하기 위한 클래스
+class ThreadDemo5 extends Thread{
+	
+	//보조작업스레드객체의 이름을 저장할 생성자
+	public ThreadDemo5(String name) {
+		super(name);
+	}
+	
+	//현재 실행되고 있는 Stack메모리영역에 올라가 있는 모든 스레드에 대한 정보를 화면에 출력하는일
+	@Override
+	public void run() {
+	/*		
+		Thread클래스의 static메소드인 getAllStackTraces()메소드 ??
+		- 프로세스 내부에서 실행하는 모든 스레드에 대한 정보를 얻을수 있습니다.
+		- 이메소드는 Map부모인터페이스타입의 자식객체를 반환하는데...
+		  key자리에는 스레드객체이고, value는  스레드의 상태 기록들을 갖고 있는 StackTraceElement[] 배열입니다.
+	*/		
+		//스택 메모리안의 현재 실행중인 모든 Thread객체(key)들과
+		//그 모든 Thread객체(key)와 같이 저장된 Thread객체의 상태기록들이 저장된
+		//자식 HashMap<Thread, StackTraceElement[]> 객체를 반환합니다.
+		Map<Thread, StackTraceElement[]> map = Thread.getAllStackTraces();
+		
+		//위 HashMap배열에 저장된 Thread객체(key)들만
+		//HashSet배열에 담아 HashSet배열메모리 주소 자체를 반환
+		Set<Thread> set = map.keySet();
+		
+		//HashSet배열에 저장된 Thread객체(key)들 모두 꺼내어
+		//Iterator배열에 담아서 Iterator배열메모리 주소 자체를 반환
+		Iterator<Thread> iterator = set.iterator();
+		
+		//JVM의 Stack메모리 영역안에 존재하는 모든 Thread객체들에 순번을 설정하기 위한 변수
+		int x = 0;
+		
+		//Iterator배열에 현재 실행중인 Thread객체(key)들이 저장되어 있으면 반복해서 작업
+		while(iterator.hasNext()) {
+			
+			//현재 실행중인 Thread객체(key)를 하나씩 얻기
+			Thread thread = iterator.next();
+			
+			//현재 실행중인 스레드객체(key)를 이용해 스레드객체(key)와 같이 저장되었던
+			//상태기록정보들이 저장된 StackTraceElement[]배열을 하나씩 얻기 
+			//위 HashMap에서 얻기
+			StackTraceElement[] ste = map.get(thread);
+			
+			//(++x) : 스택안에 있는 현재 실행되고 있는 스레드객체의 출력순서
+			//thread.getName() : 스택안에 있는 현재 실행되고 있는 스레드객체의 이름 반환
+			//thread.getThreadGroup().getName()
+			//:현재 실행되고 있는 스레드객체가 속한 스레드그룹객체의 이름 반환
+			//thread.isDaemon() : 현재 실행중인 스레드객체가 데몬스레드면 true 반환
+			//					  일반 스레드이면 fasle를 반환
+			System.out.println(
+					             "[" + (++x) + "] name :" + thread.getName() +
+					             ", grop : " + thread.getThreadGroup().getName() + 
+					             ", demon : " + thread.isDaemon()
+							   );
+			//현재 실행되고 있는 Thread객체의 상태기록 정보를 
+			//StackTraceElement[] 배열에서 반복해서 꺼내와 출력
+			for(int i=0;  i<ste.length;  i++) {
+				System.out.println(ste[i]);
+			}
+			
+			System.out.println();
+		}//while
+	}//run메소드 
+}//클래스 닫기 
 public class ThreadEx10 {
-
+	//주(메인)스레드 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new ThreadDemo4("첫번째 보조작업스레드").start();
+		new ThreadDemo5("두번째 보조작업스레드").start();
 	}
 
 }
+
+/*
+
+객체를 참조하기 위한 핸들러 역할을 하는 데몬스레드 
+[1] name : Reference Handler, group : system, demon : true
+
+자바 GUI(SWING)프로그램에서 이벤트 동작을 실행하기 위한 데몬스레드 
+[2] name : Attach Listener, group : system, demon : true
+
+[3] name : Notification Thread, group : system, demon : true
+
+보조작업스레드 ThreadDemo5 현재 실행중인 스레드 객체의 정보 출력
+[4] name : 두번째 보조작업스레드, group : main, demon : false
+
+현재 사용되고 있지 않은 객체메모리를 JVM의 heap영역에서 자동제거하는 데몬스레드 
+[5] name : Finalizer, group : system, demon : true
+
+[6] name : Signal Dispatcher, group : system, demon : true
+
+보조작업스레드 ThreadDemo4 현재 실행중인 스레드 객체의 정보 출력
+[7] name : 첫번째 보조작업스레드, group : main, demon : false
+
+
+[8] name : Common-Cleaner, group : InnocuousThreadGroup, demon : true
+
+
+
+*/
+
+
+
+
+
+
+
+
